71cd0bf6d94e542514e6c139f0236a63f36af4ec
shrijeetpawar <shrijeet.pawar_job@proton.me>
2025-05-09 23:36:36 +0530

    Render config updates

--- config/settings.py
+++ config/settings.py
@@ -79,14 +79,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'bankdb'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'shri'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
+    # 'default': {
+        
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.getenv('DB_NAME', 'bankdb'),
+    #     'USER': os.getenv('DB_USER', 'postgres'),
+    #     'PASSWORD': os.getenv('DB_PASSWORD', 'shri'),
+    #     'HOST': os.getenv('DB_HOST', 'localhost'),
+    #     'PORT': os.getenv('DB_PORT', '5432'),
+    # }
+    'default': dj_database_url.config(conn_max_age=600)
 }
 GRAPHENE = {
     'SCHEMA': 'banks.schema.schema'
